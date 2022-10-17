c3ca53232640ae413660f5a3fc4fbc819a9cdc5b
Kateryna Ostrynska <kateriinag@gmail.com>
2022-10-17 18:47:10 +0300

    Update nav-btn.js

--- js/nav-btn.js
+++ js/nav-btn.js
@@ -1,1 +1,22 @@
-const btnsContainer = document.querySelector('.buttons-list');
+const btns = document.querySelectorAll('.buttons__portfolio');
+const products = document.querySelectorAll('.product__item');
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', e => {
+    e.preventDefault();
+
+    const value = e.target.dataset.value;
+    console.log(value);
+    products.forEach(product => {
+      if (value == 'all') {
+        product.style.display = 'block';
+      } else {
+        if (product.classList.contains(value)) {
+          product.style.display = 'block';
+        } else {
+          product.style.display = 'none';
+        }
+      }
+    });
+  });
+}
