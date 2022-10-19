4d442f4e657077ce934736ea4fe0f55a0770a58b
Kateryna Ostrynska <kateriinag@gmail.com>
2022-10-19 21:42:32 +0300

    Update nav-btn.js

--- js/nav-btn.js
+++ js/nav-btn.js
@@ -1,22 +1,35 @@
 const btns = document.querySelectorAll('.buttons__portfolio');
 const products = document.querySelectorAll('.product__item');
 
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', e => {
-    e.preventDefault();
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener('click', e => {
+//     e.preventDefault();
 
-    const value = e.target.dataset.value;
-    console.log(value);
-    products.forEach(product => {
-      if (value == 'all') {
-        product.style.display = 'block';
-      } else {
-        if (product.classList.contains(value)) {
-          product.style.display = 'block';
-        } else {
-          product.style.display = 'none';
-        }
-      }
-    });
-  });
-}
+//     const value = e.target.dataset.value;
+//     // console.log(value);
+//     btns.forEach(btn => {
+//       if (value === '') {
+//         product.style.display = 'block';
+//       }
+//     });
+//   });
+// }
+
+// const rating = document.getElementById('rating');
+// const elements = document.querySelectorAll('li');
+
+// rating.addEventListener('change', function () {
+//   let value = rating.value;
+//   [...elements].forEach(element => {
+//     if (value === '') {
+//       element.classList.remove('hidden');
+//     } else {
+//       const rating = element.dataset.rating;
+//       if (!rating || rating < value) {
+//         element.classList.add('hidden');
+//       } else {
+//         element.classList.remove('hidden');
+//       }
+//     }
+//   });
+// });
