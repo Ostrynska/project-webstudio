f3f7b9e28914730a448804ae0bb61bdaab9764a0
Kateryna Ostrynska <kateriinag@gmail.com>
2022-10-31 20:15:24 +0200

    Create change-lng.js

---
+++ js/change-lng.js
@@ -0,0 +1,24 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'ua'];
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + '#en';
+    location.reload();
+  }
+
+  select.value = hash;
+  document.querySelector('studio').innerHTML = langArr['studio'][hash];
+}
+
+// :hangeLanguage();
